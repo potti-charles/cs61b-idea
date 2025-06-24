4e8e26008f759b03f65442052996655e3d915fb8
potti_charles <skygentleman@gmail.com>
2025-06-24 15:35:01 +0800

    Initial commit with local .gitignore

---
+++ src/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x;
+        x = 0;
+        while (x < 10) {
+            System.out.println(x);
+            x = x + 1;
+        }
+
+        String y = "horse";
+    }
+}
